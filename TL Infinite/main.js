bf483c857e9612193fcac9b534418dd9d8243b32
Shinar0 <Shinar0.Blubb@gmail.com>
2022-10-22 14:22:45 +0200

    OhMyBabiba

--- TL Infinite/main.js
+++ TL Infinite/main.js
@@ -1,64 +1,160 @@
-/* var Ember_selection = 0;
-var result = $.csv.toObjects(claw.csv);
+var mode_selection = "craft";
+var ember_selection = "Cultivation Ember";
+var type_selection = "Claw";
+var item_level = 87;
+let selection = Claw_craft;
+var number_of_prefixes = 0;
+var number_of_suffixes = 0;
+var number_of_affixes = number_of_prefixes + number_of_suffixes;
+var myMin = 0;
+var myMax = 0;
+var myMin2 = 0;
+var myMax2 = 0;
+var myMin3 = 0;
+var myMax3 = 0;
+var range = 0;
+var range2 = 0;
+var range3 = 0;
 
-function rando() {
+function random_mod() {
   var mod_selection = [];
   var mod_selection_weight = [];
-  
-  
-  for (i=0;i<Embers[Ember_selection].Affixes.length;i++) {
-    mod_selection[i] = i
-    mod_selection_weight[i] = Embers[Ember_selection].Affixes[i].mod_weight
+  var z = 0;
+  for (i = 0; i < selection.length; i++) {
+    if (
+      selection[i].Ember == ember_selection &&
+      item_level >= selection[i].Level
+    ) {
+      if (
+        selection[i].Modifier_range.length == 0 &&
+        selection[i].Type.length == 1
+      ) {
+        mod_selection[z] = ["T" + selection[i].Tier + selection[i].Modifier, i];
+        mod_selection_weight[z] = selection[i].Weight;
+        z++;
+      } else if (
+        selection[i].Modifier_range.length == 2 &&
+        selection[i].Type.length == 1
+      ) {
+        myMin = selection[i].Modifier_range[0];
+        myMax = selection[i].Modifier_range[1];
+        mod_selected_range =
+          Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+        mod_selection[z] = [
+          "T" +
+            selection[i].Tier +
+            " " +
+            mod_selected_range +
+            selection[i].Type[0],
+          i,
+          mod_selected_range,
+        ];
+        mod_selection_weight[z] = selection[i].Weight;
+        z++;
+      } else if (
+        selection[i].Modifier_range.length == 4 &&
+        selection[i].Type.length == 2
+      ) {
+        myMin = selection[i].Modifier_range[0];
+        myMax = selection[i].Modifier_range[1];
+        myMin2 = selection[i].Modifier_range[2];
+        myMax2 = selection[i].Modifier_range[3];
+        range = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+        range2 = Math.floor(Math.random() * (myMax2 - myMin2 + 1)) + myMin2;
+        mod_selection[z] = [
+          "T" +
+            selection[i].Tier +
+            " " +
+            range +
+            selection[i].Type[0] +
+            range2 +
+            selection[i].Type[1],
+          i,
+          range,
+          range2,
+        ];
+        mod_selection_weight[z] = selection[i].Weight;
+        z++;
+      } else if (
+        selection[i].Modifier_range.length == 2 &&
+        selection[i].Type.length == 2
+      ) {
+        myMin = selection[i].Modifier_range[0];
+        myMax = selection[i].Modifier_range[1];
+        mod_selected_range =
+          Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+        mod_selection[z] = [
+          "T" +
+            selection[i].Tier +
+            " " +
+            selection[i].Type[0] +
+            mod_selected_range +
+            selection[i].Type[1],
+          i,
+          mod_selected_range,
+        ];
+        mod_selection_weight[z] = selection[i].Weight;
+        z++;
+      } else if (
+        selection[i].Modifier_range.length == 4 &&
+        selection[i].Type.length == 3
+      ) {
+        myMin = selection[i].Modifier_range[0];
+        myMax = selection[i].Modifier_range[1];
+        myMin2 = selection[i].Modifier_range[2];
+        myMax2 = selection[i].Modifier_range[3];
+        range = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+        range2 = Math.floor(Math.random() * (myMax2 - myMin2 + 1)) + myMin2;
+        mod_selection[z] = [
+          "T" +
+            selection[i].Tier +
+            " " +
+            selection[i].Type[0] +
+            range +
+            selection[i].Type[1] +
+            range2 +
+            selection[i].Type[2],
+          i,
+          range,
+          range2,
+        ];
+        mod_selection_weight[z] = selection[i].Weight;
+        z++;
+      } else if (
+        selection[i].Modifier_range.length == 6 &&
+        selection[i].Type.length == 3
+      ) {
+        myMin = selection[i].Modifier_range[0];
+        myMax = selection[i].Modifier_range[1];
+        myMin2 = selection[i].Modifier_range[2];
+        myMax2 = selection[i].Modifier_range[3];
+        myMin3 = selection[i].Modifier_range[4];
+        myMax3 = selection[i].Modifier_range[5];
+        range = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
+        range2 = Math.floor(Math.random() * (myMax2 - myMin2 + 1)) + myMin2;
+        range3 = Math.floor(Math.random() * (myMax3 - myMin3 + 1)) + myMin3;
+        mod_selection[z] = [
+          "T" +
+            selection[i].Tier +
+            " " +
+            range +
+            selection[i].Type[0] +
+            range2 +
+            selection[i].Type[1] +
+            range3 +
+            selection[i].Type[2],
+          i,
+          range,
+          range2,
+          range3,
+        ];
+        mod_selection_weight[z] = selection[i].Weight;
+        z++;
+      }
+    }
   }
-  
   chosen_mod = chance.weighted(mod_selection, mod_selection_weight);
-
-  var chosen_tier_mod_descriptions = [];
-  var chosen_tier_weights = [];
-
-  for(i=0; i<Embers[Ember_selection].Affixes[chosen_mod].Tiers.length; i++) {
-    chosen_tier_mod_descriptions[i] = "T" + Embers[Ember_selection].Affixes[chosen_mod].Tiers[i].Tier + " " + Embers[Ember_selection].Affixes[chosen_mod].Tiers[i].mod_tier_prefix + Embers[Ember_selection].Affixes[chosen_mod].Tiers[i].mod_tier_range[Math.floor(Math.random()*Embers[Ember_selection].Affixes[chosen_mod].Tiers[i].mod_tier_range.length)] + Embers[Ember_selection].Affixes[chosen_mod].Affix_Name;
-    chosen_tier_weights[i] = Embers[Ember_selection].Affixes[chosen_mod].Tiers[i].tier_weight
-  };
-
-  mod_outcome = chance.weighted(chosen_tier_mod_descriptions, chosen_tier_weights);
-  
-  console.log(mod_outcome); 
-} */
-
-const Claw_craft = [
-  {
-    Tier: 1,
-    Modifier: "(8–10) mana restored on hit",
-    Level: 85,
-    Weight: 171,
-    Ember: "Vigor Ember",
-    Affix_Type: "Prefix",
-  },
-  {
-    Tier: 2,
-    Modifier: "Give this gear (11–12) - (13–15) Erosion Damage",
-    Level: 80,
-    Weight: 514,
-    Ember: "Vigor Ember",
-    Affix_Type: "Prefix",
-  },
-  {
-    Tier: 3,
-    Modifier: "1 - 5 added attack lightning damage per 12 dexterity",
-    Level: 75,
-    Weight: 1543,
-    Ember: "Vigor Ember",
-    Affix_Type: "Prefix",
-  }
-]
-
-var test1 = Claw_craft[0].Modifier;
-var regex = /\d+/g
-var result = test1.match(regex);
-var myMin = parseInt(result[0]);
-var myMax = parseInt(result[1]);
-
-var random_number = Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
-
-console.log(random_number);
+  console.log(mod_selection);
+  console.log(mod_selection_weight);
+  console.log(chosen_mod[0], chosen_mod[1], chosen_mod[2]);
+}
